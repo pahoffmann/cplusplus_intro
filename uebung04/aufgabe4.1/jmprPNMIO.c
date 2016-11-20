9fe7cebea92d108e02d8313db36bf3151ae7bc86
Patrick <pahoffmann@uni-osnabrueck.de>
2016-11-20 19:50:10 +0100

    Refreshed 4.1

--- uebung04/aufgabe4.1/jmprPNMIO.c
+++ uebung04/aufgabe4.1/jmprPNMIO.c
@@ -1,64 +1,11 @@
-#include "jmprPNMIO.h"
-#include <stdio.h>
 #include <SDL2/SDL.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "jmprPNMIO.h"
 
 enum jmpr_PNMMode mode;
 
-SDL_Surface* jmpr_surfaceFromImage(jmpr_Image* img){
-        SDL_Surface* surface;
-        int width;
-        int height;
-        int pitch;
-        Uint32 rmask;
-        Uint32 gmask;
-        Uint32 bmask;
-        Uint32 amask;
-        unsigned char *pixels;
 
-        int i;
-        int j;
-        int k;
-
-        k=0;
-
-
-        width = img->w;
-        height = img->h;
-        pitch = width * 3;
-        rmask = 0x000000ff;
-        gmask = 0x0000ff00;
-        bmask = 0x00ff0000;
-        amask = 0xff000000;
-        pixels = (unsigned char *)malloc(height*width*3*sizeof(unsigned char));
-
-
-        for(i=0; i < height; i++){
-                for(j=0; j < width; j++){
-                        pixels[k] = (unsigned char) img->pixels[i][j].r;
-                        k++;
-                        pixels[k] = (unsigned char) img->pixels[i][j].g;
-                        k++;
-                        pixels[k] = (unsigned char) img->pixels[i][j].b;
-                        k++;
-                }
-        }
-        surface = SDL_CreateRGBSurfaceFrom(pixels,width,height,24,pitch,rmask,gmask,bmask,amask);
-
-        return surface;
-}
-
-
-
-
-
-
-
-
-void jmpr_savePNM(char* filename, jmpr_Image* img, enum jmpr_PNMMode mode){
-}
 
 
 void printTest(jmpr_Pixel** image,int h,int w){
@@ -211,46 +158,3 @@ void jmpr_freeImage(jmpr_Image* img){
 	
 }
 
-int main(int argc, char* argv[]){
-	
-	char* filename;
-	jmpr_Image* image;
-        SDL_Surface* surface;
-        SDL_Window* window;
-        SDL_Texture* texture;
-        SDL_Renderer* renderer;
-	
-	filename = "/home/patrick/UniOsnabrück/c++/uebungen/uebung4/panorama1.pnm";
-	printf("%s\n",filename);
-	image = jmpr_readImage(filename);
-        if((SDL_Init(SDL_INIT_VIDEO)) != 0){
-        fprintf(stderr, "%s\n", SDL_GetError());
-                return -1;
-        }
-
-        window = SDL_CreateWindow("TEST", 0, 0, (image->w)/3,1133/3, SDL_WINDOW_SHOWN);
-
-        if(NULL == window){
-            fprintf(stderr, "%s\n", SDL_GetError());
-            return -1;
-        }
-
-        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-        if(NULL == renderer){
-            fprintf(stderr, "%s\n", SDL_GetError());
-            return -1;
-        }
-	
-        surface = jmpr_surfaceFromImage(image);
-        texture = SDL_CreateTextureFromSurface(renderer,surface);
-        SDL_RenderCopy(renderer,texture,0,0);
-        SDL_RenderPresent(renderer);
-        SDL_Delay(10000);
-        SDL_DestroyWindow(window);
-        SDL_DestroyRenderer(renderer);
-        SDL_Quit();
-	
-	
-	return 0;
-}
