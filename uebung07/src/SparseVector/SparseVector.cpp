a87e95bcf8b75450f4ab9ea9e6df8c97ec2ddcd7
szielinski <szielinski@uni-osnabrueck.de>
2016-12-11 17:51:34 +0100

    Update von SparseVector

--- uebung07/src/SparseVector/SparseVector.cpp
+++ uebung07/src/SparseVector/SparseVector.cpp
@@ -6,6 +6,51 @@
 *         *
 **********/
 
+
+/**
+ * @brief adds a nonzero element to the list according to its index
+ */
+void SparseVector::setNonzeroElem(int index, int value){
+	node *temp;
+	node *next;
+	temp = start;
+	while(temp->next != NULL){
+		if(temp->index > index){
+			break;
+		}
+
+		next = temp->next;
+		delete temp;
+		temp = next;
+
+	}
+
+	if(temp->next == NULL){
+		temp->next = new node(index, value, 0);
+	} else {
+		next = temp->next;
+		temp->next = new node (index, value, next);
+	}
+}
+
+
+/**
+ * @brief removes an element from the list
+ */
+void SparseVector::removeElem(int index){
+	node *temp;
+	node *next;
+	temp = start;
+
+	while(temp){
+		if (temp->index == index){
+			next = temp->next;
+			delete temp;
+			temp = next;
+		}
+	}
+}
+
 void SparseVector::clear(){
 
 	node *actual=start;
@@ -20,23 +65,21 @@ void SparseVector::clear(){
 void SparseVector::copy(const SparseVector &other){
 	size = other.size;
 	actSize = other.actSize;
-	if(!other.start){
-                this->start = 0;
-        }
-        else if(this != &other){
 
-                node *copy = start;
-                node *original = other.start;
-                while(original){
+	if (!other.start){
+    	this->start = 0;
+	} else if (this != &other){
+	  node *copy = start;
+	  node *original = other.start;
 
-                        copy = new node(original->index,original->value,0);
-                        copy = copy->next;
-                        original = original->next;
-
-                }
-        }else{
-                this->start = other.start;
-        }
+	  while(original){
+		  copy = new node(original->index,original->value,0);
+		  copy = copy->next;
+		  original = original->next;
+	  }
+  } else {
+	  this->start = other.start;
+  }
 
 }
 
@@ -57,20 +100,46 @@ SparseVector::SparseVector(int size) /*: start(0,0,0)*/{
 
 /* Copy - Constructor */
 SparseVector::SparseVector(const SparseVector &other){
-	
+
 	copy(other);
-	
+
+}
+
+int SparseVector::getElem(int index){
+	if (this->actSize == 0){
+		std::cerr << "No element in list" << std::endl;
+		return 0;
+	}
+
+	node *temp;
+	node *next;
+	temp = start;
+	while(temp){
+		if(temp->index == index){
+			return temp->value;
+			break;
+		}
+		next = temp->next;
+		delete temp;
+		temp = next;
+	}
+
+	return -1;
+}
+
+void SparseVector::setElem(int index, int value){
+	setNonzeroElem(index, value);
 }
 
 
 /* = operator */
 
-SparseVector& SparseVector::operator=(const SparseVector &other){ 
-	
+SparseVector& SparseVector::operator=(const SparseVector &other){
+
 	if(this != &other){
 		clear();
 		copy(other);
-	}	
+	}
 	return *this;
 }
 
@@ -78,6 +147,14 @@ SparseVector& SparseVector::operator=(const SparseVector &other){
 
 int SparseVector::getSize(){
 	return size;
+}
+
+bool operator ==(const SparseVector &first, const SparseVector &second){
+
+}
+
+bool operator !=(const SparseVector &first, const SparseVector &second){
+	
 }
 
 
--- uebung07/src/SparseVector/SparseVector.hpp
+++ uebung07/src/SparseVector/SparseVector.hpp
@@ -4,7 +4,7 @@ class SparseVector{
 private:
 
 	struct node{
-	
+
 		int index;
 		int value;
 		node *next;
@@ -27,18 +27,33 @@ private:
 
 	void copy(const SparseVector &other);
 
+	void setNonzeroElem(int index, int value);
+
+	void removeElem(int index);
+
 public:
 
 	SparseVector(int size);
-	
-	SparseVector(const SparseVector &other);	
+
+	SparseVector(const SparseVector &other);
 
 	int getSize();
-	
+
+	/** Warum ist diese Funktion nicht auf dem Aufgabenblatt gefordert???
+	 * @brief returns value of element listed at index
+	 */
+	int getElem(int index);
+
+	void setElem(int index, int value);
+
 	SparseVector& operator=(const SparseVector &other);
-	
+
+	bool operator==(const SparseVector &first, const SparseVector &second);
+
+	bool operator!=(const SparseVector &first, const SparseVector &second);
+
 	~SparseVector();
-	
+
 
 
 };
