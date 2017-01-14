71460422f0b1323b017a485778ef32d0e136a3ad
Patrick <pahoffmann@uni-osnabrueck.de>
2017-01-14 20:33:34 +0100

    update!

--- uebung10/aufgabe2/src/Aufgabe2_2.cpp
+++ uebung10/aufgabe2/src/Aufgabe2_2.cpp
@@ -5,11 +5,16 @@
 class Aufgabe2_2{
 public:
 	std::string operator()(){
+
+		 char chars[] =
+		 "0123456789"
+		 "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		 "abcdefghijklmnopqrstuvwxyz";
 		
 		int size = 5 + ((rand()+1) % 6 );
 		std::string str;
 		for(int i = 0; i < size; i++){
-			str+=(char)('a' + rand()%26);
+			str+=chars[(rand()+1) % (sizeof(chars)/sizeof(*chars))];
 		}
 		
 		return str;
