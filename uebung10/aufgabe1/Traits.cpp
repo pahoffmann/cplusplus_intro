39a45c37c9e4b3f6a8e6917bde5cb473a04c6392
Patrick <pahoffmann@uni-osnabrueck.de>
2017-01-14 22:40:37 +0100

    Added Traits!

--- uebung10/aufgabe1/Traits.cpp
+++ uebung10/aufgabe1/Traits.cpp
@@ -13,6 +13,8 @@ struct GVertex
 {
 	GVertex(T x, T y, T z) : m_x(x), m_y(y), m_z(z) {}
 
+	virtual void f(){};
+
 	T m_x;	// x coordinate
 	T m_y;	// y coordinate
 	T m_z;	// z coordinate
@@ -21,14 +23,11 @@ struct GVertex
 /// A struct to represent a vertex with color
 /// values
 template<typename T, typename C>
-struct CVertex
+struct CVertex : public GVertex<T>
 {
 	CVertex(T x, T y, T z, C r, C g, C b)
-		: m_x(x), m_y(y), m_z(z), m_r(r), m_g(g), m_b(b) {}
+		: GVertex<T>(x,y,z), m_r(r), m_g(g), m_b(b) {}
 
-	T m_x;	// x coordinate
-	T m_y;	// y coordinate
-	T m_z;	// z coordinate
 	C m_r;	// red color component
 	C m_g;	// green color component
 	C m_b;	// blue color component
@@ -57,12 +56,28 @@ struct VertexTraits<ColorVertex>
 template<typename T>
 void print_vertex(T& vertex)
 {
-	// TODO: Extend and use the traits object to
-	// safely access the color information of the
-	// vertex type (only if supported!) and print
-	// position and color if present
+	std::cout << vertex.m_x << " " << vertex.m_y << " " << vertex.m_z << std::endl;
+	std::cout << "COLOR?" << std::endl;
 
+
+	if(VertexTraits<T>::HasColor){
+		std::cout << "YES!" <<std::endl;
+		int r = (dynamic_cast<ColorVertex&>(vertex)).m_r;
+		int g = (dynamic_cast<ColorVertex&>(vertex)).m_g;
+		int b = (dynamic_cast<ColorVertex&>(vertex)).m_b;
+		
+		std::cout << r << " " << g << " " << b << std::endl;
+		 
+	} else std::cout << "NO!" << std::endl;
+	
 }
+
+/*template <>
+void print_vertex(ColorVertex& vertex){
+	std::cout << "COLOR" << std::endl;
+	std::cout << vertex.m_x << " " << vertex.m_y << " " << vertex.m_z << std::endl;
+	std::cout << vertex.m_r << " " << vertex.m_g << " " << vertex.m_b << std::endl;
+}*/
 
 int main(int argc, char** argv)
 {
